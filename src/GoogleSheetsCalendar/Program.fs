bb97c5418f59b5d9d8705dbb9c10a1cf987ce133
Gennadii Saltyshchak <GennadyGS@gmail.com>
2023-02-18 18:12:06 +0200

    Read the configuration and create the service

--- src/GoogleSheetsCalendar/Program.fs
+++ src/GoogleSheetsCalendar/Program.fs
@@ -1,2 +1,46 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+open System.IO
+open System.Reflection
+open Microsoft.Extensions.Configuration
+open Google.Apis.Auth.OAuth2
+open Google.Apis.Services
+open Google.Apis.Sheets.v4
+
+module NullCoalesce =
+    let coalesce (b: 'a Lazy) (a: 'a) =
+        if obj.ReferenceEquals(a, null) then b.Value else a
+
+[<CLIMutable>]
+type Configuration = { SpreadsheetId: string }
+
+[<Literal>]
+let credentialsFileName = "credentials.json"
+
+let executableFilePath = Assembly.GetExecutingAssembly().Location
+let executableDirectoryPath = Path.GetDirectoryName(executableFilePath)
+
+let configuration =
+    let nativeConfiguration =
+        (new ConfigurationBuilder())
+            .SetBasePath(executableDirectoryPath)
+            .AddJsonFile("appsettings.json", true, true)
+            .AddUserSecrets<Configuration>()
+            .Build()
+
+    nativeConfiguration.Get<Configuration>()
+    |> NullCoalesce.coalesce (lazy raise (InvalidOperationException("Configuration is missing")))
+
+let credentialFileName = Path.Combine(executableDirectoryPath, credentialsFileName)
+
+if not (File.Exists(credentialFileName)) then
+    raise (InvalidOperationException($"File {credentialsFileName} is missing"))
+
+let credential =
+    GoogleCredential
+        .FromFile(credentialFileName)
+        .CreateScoped([| SheetsService.Scope.Spreadsheets |])
+
+let initializer =
+    new BaseClientService.Initializer(HttpClientInitializer = credential)
+
+let sheetsService = new SheetsService(initializer)
